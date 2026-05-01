632894531f29b74f97dbd84ca128a5195957117d
SHANAWAJ NADAF <147726542+shanawaj679@users.noreply.github.com>
2026-05-01 13:49:28 +0530

    my first backend api

---
+++ ch1/backend.js
@@ -0,0 +1,10 @@
+ const express=require("express");
+ const cors=require("cors")
+    const app=express();
+     app.use(cors());
+    app.get("/",(req,res)=>{
+        res.send("Hello world");
+    })
+    app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
